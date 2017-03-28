25758b191c6714e73417c2a7a769cdfc6ad82de6
Leonardo Perez <find.leonardo@gmail.com>
2017-03-28 11:19:50 -0400

    Clean up and refactored.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Notifier = require('./notifier');
-const tracker = require('./tracker');
+const Tracker = require('./tracker');
 
 // @Official_PAX Twitter ID: 26281970 @leonardojperez ID: 140543363
 var streamIds = process.env.STREAM_IDS || '26281970, 140543363';
@@ -11,25 +11,19 @@ var keywords = process.env.KEYWORDS || ['ticket', 'badge', 'seattle', 'sale', 'w
 
 // Heroku Specific server binding.
 var http = require('http');
-http.createServer(function (request, response) {}).listen(process.env.PORT || 5000);
+http.createServer(function (request, response) { }).listen(process.env.PORT || 5000);
 
-tracker(streamIds, keywords, function (error, tweet) {
+Tracker.track(streamIds, keywords, function (error, tweet) {
     if (error) {
         return console.log(error);
     }
 
-    const tweetText = tweet
-        .text
-        .toLowerCase();
-
-    const isValidChannel = tweet
-        .user
-        .screen_name
-        .toLowerCase() === screenName.toLowerCase();
+    const tweetText = tweet.text.toLowerCase();
+    const isCorrectChannel = tweet.user.screen_name.toLowerCase() === screenName.toLowerCase();
 
     const notify = keywords.findIndex(function (kw) {
         return tweetText.indexOf(kw) !== -1;
-    }) !== -1 && isValidChannel;
+    }) !== -1 && isCorrectChannel;
 
     if (notify) {
         Notifier.sendMessage(tweetText);
--- notifier.js
+++ notifier.js
@@ -1,28 +1,28 @@
 'use strict';
 
-var sid = 'ACfa9294cb5a11c1a81b0ff18b7d4b6d5b';
-var t = '4c21e947cab43ae4885079d8f2756e96';
-var client = require('twilio')(sid, t);
+var SENDING_NUMBER = '+17873392453';
+var SID = 'ACfa9294cb5a11c1a81b0ff18b7d4b6d5b';
+var T = '4c21e947cab43ae4885079d8f2756e96';
+var client = require('twilio')(SID, T);
 
 var bros = require('./numbers').people;
-var sendingNumber = '+17873392453';
 
 module.exports.sendMessage = function (message) {
-    bros
-        .forEach(function (geek) {
-            client
-                .messages
-                .create({
-                    body: `\nHey ${geek.name},\n${message}\nQuick: http://west.paxsite.com/`,
-                    to: geek.number,
-                    from: sendingNumber
-                }, function (err, data) {
-                    if (err) {
-                        console.error('Could not notify ' + geek.name);
-                        console.error(err);
-                    } else {
-                        console.log(geek.name + ' notified!');
-                    }
-                });
+    bros.forEach(function (geek) {
+
+        const sms = {
+            body: `\nHey ${geek.name},\n${message}\nQuick: http://west.paxsite.com/`,
+            to: geek.number,
+            from: SENDING_NUMBER
+        };
+
+        client.messages.create(sms, function (err, data) {
+            if (err) {
+                console.error('Could not notify ' + geek.name);
+                console.error(err);
+            } else {
+                console.log(geek.name + ' notified!');
+            }
         });
+    });
 };
