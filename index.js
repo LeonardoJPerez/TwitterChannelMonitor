5e9ecf0f6037e8ad70963210694f2a3efd92a626
Leonardo Perez <find.leonardo@gmail.com>
2017-03-27 22:19:21 -0400

    Removde my twitter id.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var Notifier = require('./notifier');
 var tracker = require('./tracker');
 
-//@Official_PAX Twitter ID: 26281970
-var streamIds = process.env.STREAM_IDS || '26281970, 140543363';
+// @Official_PAX Twitter ID: 26281970
+// @leonardojperez ID: 140543363
+var streamIds = process.env.STREAM_IDS || '26281970';
 var keywords = process.env.KEYWORDS || ['ticket', 'badge', 'seattle', 'sale'];
 
 tracker(streamIds, keywords, function (error, tweet) {
