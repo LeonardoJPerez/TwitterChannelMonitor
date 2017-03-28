0be73d7cc612716817fd0e6d8a1193460d89d45c
Leonardo Perez <find.leonardo@gmail.com>
2017-03-27 22:05:49 -0400

    Clean up.

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ var streamIds = process.env.STREAM_IDS || '26281970, 140543363';
 var keywords = process.env.KEYWORDS || ['ticket', 'badge', 'seattle', 'sale'];
 
 tracker(streamIds, keywords, function (error, tweet) {
-    if (error) { return console.log(error); }
+    if (error) {
+        return console.log(error);
+    }
 
     let tweetText = tweet.text;
     var notify = keywords.findIndex(function (kw) {
         return tweetText.indexOf(kw) !== -1;
     }) !== -1;
 
-    if (notify) { Notifier.sendMessage(tweetText); }
+    if (notify) {
+        Notifier.sendMessage(tweetText);
+    }
 });
