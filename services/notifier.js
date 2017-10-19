f27c8255f2e703764bbfb67737fd7d675f03163c
Leonardo J. Perez <find.leonardo@gmail.com>
2017-10-19 15:31:31 -0400

    Clean up

--- services/notifier.js
+++ services/notifier.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const SENDING_NUMBER = '+17873392453';
-const SID = 'ACfa9294cb5a11c1a81b0ff18b7d4b6d5b';
-const T = '4c21e947cab43ae4885079d8f2756e96';
+const SENDING_NUMBER = '+';
+const SID = '';
+const T = '';
 const client = require('twilio')(SID, T);
 
 const bros = require('./numbers').people;
